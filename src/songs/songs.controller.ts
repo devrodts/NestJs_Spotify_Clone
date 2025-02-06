ca9125de2923c0c1443b5fcef6a40d33327a65e1
rod <dev_rodts@outlook.com>
2025-02-06 12:33:57 -0300

    "feat: add songs controller with basic CRUD operations"

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Get, Param, Put, Body } from '@nestjs/common';
+import { Controller, Get, Param, Put, Body, Delete, Post } from '@nestjs/common';
+import { UpdateSongDto } from './dto/update-song.dto';
+import { CreateSongDto } from './dto/create-song.dto';
+
 
 @Controller('songs')
 
 export class SongsController {
 
+    @Post()
+    create(@Body() createSongDto: CreateSongDto): string {
+        return `This action adds a new song ${createSongDto.title}`;
+    }
+
     @Get()
     findAll(): string {
         return 'This action returns all songs';
+    }
+
+
+    @Get(':id')
+    findOne(@Param('id') id: string): string {
+        return `This action returns a #${id} song`;
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() updateSongDto: UpdateSongDto): string {
+        return `This action updates a #${id} song`;
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string): string {
+        return `This action removes a #${id} song`;
     }
 
 }
