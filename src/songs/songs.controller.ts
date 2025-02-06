7750e6555042d5230d62cc35bc7e591b73529062
rod <dev_rodts@outlook.com>
2025-02-06 13:29:43 -0300

    "feat: update songs controller"

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,36 +1,48 @@
-import { Controller, Get, Param, Put, Body, Delete, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Put,
+  Body,
+  Delete,
+  Post,
+} from '@nestjs/common';
 import { UpdateSongDto } from './dto/update-song.dto';
 import { CreateSongDto } from './dto/create-song.dto';
-
+import { SongsService } from './songs.service';
+import { FindAllSongsDto } from './dto/find-all-songs.dto';
 
 @Controller('songs')
-
 export class SongsController {
+    
+  constructor(private readonly songsService: SongsService) {}
 
-    @Post()
-    create(@Body() createSongDto: CreateSongDto): string {
-        return `This action adds a new song ${createSongDto.title}`;
-    }
+  @Post()
+  create(@Body() createSongDto: CreateSongDto) {
+    this.songsService.create(createSongDto);
+  }
 
-    @Get()
-    findAll(): string {
-        return 'This action returns all songs';
-    }
+  @Get()
+  findAll(): FindAllSongsDto[] {
+    return this.songsService.findAll();
+  }
 
 
-    @Get(':id')
-    findOne(@Param('id') id: string): string {
-        return `This action returns a #${id} song`;
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id} song`;
+  }
 
-    @Put(':id')
-    update(@Param('id') id: string, @Body() updateSongDto: UpdateSongDto): string {
-        return `This action updates a #${id} song`;
-    }
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateSongDto: UpdateSongDto,
+  ): string {
+    return `This action updates a #${id} song`;
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string): string {
-        return `This action removes a #${id} song`;
-    }
-
+  @Delete(':id')
+  remove(@Param('id') id: string): string {
+    return `This action removes a #${id} song`;
+  }
 }
