6bd6e0aaf19d4b0f8a84a26fc21d3138a8856927
rod <dev_rodts@outlook.com>
2025-02-07 09:41:24 -0300

    feat: create Logger Middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
+import { MiddlewareBuilder } from '@nestjs/core';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 
 @Module({
   imports: [SongsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareBuilder) {
+    consumer.apply(LoggerMiddleware).forRoutes('songs');
+  }
+}
